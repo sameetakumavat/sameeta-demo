347f158d038f2b2bc1e24b554ab9e2f8dc6d8f6b
sameetakumavat <sameetakumavat05@gmail.com>
2024-07-04 17:30:19 +0530

    changed the guessing game from 20 to 30

--- guessing_game.py
+++ guessing_game.py
@@ -1,19 +1,19 @@
 import random
 
 def number_guessing_game():
-    number_to_guess = random.randint(1, 20)
+    number_to_guess = random.randint(1, 30)
     attempts = 0
 
     print("Welcome to the Number Guessing Game!")
-    print("I have selected a number between 1 and 20. Can you guess what it is?")
+    print("I have selected a number between 1 and 30. Can you guess what it is?")
 
     while True:
         try:
             guess = int(input("Enter your guess: "))
             attempts += 1
 
-            if guess < 1 or guess > 20:
-                print("Please guess a number between 1 and 20.")
+            if guess < 1 or guess > 30:
+                print("Please guess a number between 1 and 30.")
                 continue
 
             if guess < number_to_guess:
